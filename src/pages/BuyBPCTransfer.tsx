9943596780b43bdb9a0eb37264ff03cef7ee05c5
martinvonson-star <martinvonson@gmail.com>
2026-01-02 16:33:58 +0100

    Update BuyBPCTransfer.tsx

--- src/pages/BuyBPCTransfer.tsx
+++ src/pages/BuyBPCTransfer.tsx
@@ -50,7 +50,7 @@ const BuyBPCTransfer = () => {
       </div>
 
       <div className="flex flex-col items-center p-6 mb-6">
-        <h1 className="text-4xl font-bold mb-2">NGN 6,500</h1>
+        <h1 className="text-4xl font-bold mb-2">NGN 10,000</h1>
         <p className="text-gray-600">{email}</p>
       </div>
 
@@ -68,11 +68,11 @@ const BuyBPCTransfer = () => {
         <div className="mb-4">
           <p className="text-gray-500 text-sm">Amount</p>
           <div className="flex justify-between items-center">
-            <p className="text-2xl font-bold">NGN 6,500</p>
+            <p className="text-2xl font-bold">NGN 10,000</p>
             <Button 
               variant="default" 
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-2"
-              onClick={() => handleCopy("6500", "Amount")}
+              onClick={() => handleCopy("10000", "Amount")}
             >
               <Copy size={18} />
               Copy
@@ -83,11 +83,11 @@ const BuyBPCTransfer = () => {
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-2xl font-bold">6503731668</p>
+            <p className="text-2xl font-bold">7078631808/p>
             <Button 
               variant="default" 
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-2"
-              onClick={() => handleCopy("6503731668", "Account Number")}
+              onClick={() => handleCopy("7078631808", "Account Number")}
             >
               <Copy size={18} />
               Copy
@@ -102,7 +102,7 @@ const BuyBPCTransfer = () => {
 
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Name</p>
-          <p className="text-2xl font-bold">BADAMASI ABDULLAHI</p>
+          <p className="text-2xl font-bold">ORIRE AKINMUSERE</p>
         </div>
       </div>
 
